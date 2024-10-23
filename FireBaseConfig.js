ff80b0a96b5b5eb671ec7bf7d087555b2b9d1206
Fedor <134657633+federalbyro@users.noreply.github.com>
2024-10-23 22:13:34 +0300

    Update FireBaseConfig.js

--- FireBaseConfig.js
+++ FireBaseConfig.js
@@ -8,15 +8,7 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAcmKCRPF0n6IKoTP3DTtBK0sBBDqfjoeQ",
-  authDomain: "to-do-app-e0477.firebaseapp.com",
-  projectId: "to-do-app-e0477",
-  storageBucket: "to-do-app-e0477.appspot.com",
-  messagingSenderId: "527608598334",
-  appId: "1:527608598334:web:c25a55807d3466b809266f",
-  measurementId: "G-L4G7HBJPBY"
-};
+
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
